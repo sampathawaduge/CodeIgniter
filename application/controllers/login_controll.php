b09344375d9a9118e9827f612b9c92009b530ed2
sameera <sampathawaduge94@gmail.com>
2016-03-23 09:03:25 +0530

    New Edited
    Update version

--- application/controllers/login_controll.php
+++ application/controllers/login_controll.php
@@ -9,8 +9,30 @@
     	}
     	public function index()
     	{
-    		$this->load->view("submit");
+            $this->load->model('Submission');
+            $query=$this->Submission->student_subcategories();
+            if($query)
+            {
+                $data['test']=$query;
+                $this->load->view("submit",$data);
+            }
+
 
     	}
+        public function show()
+        {
+            $comment=$this->input->post('comment');
+            $subcat=$this->input->post('subcat');
+
+            $array=[
+                'description'=>$comment,
+                'subcategory'=>$subcat,
+                'user'=>'test',
+                'submission_id'=>1
+            ];
+            $this->load->model("Submission");
+            $this->Submission->insert_submission($array);
+        
+        }
     	
     }
